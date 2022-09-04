5b2517d1a6e6941bca12a25b837d5a65a3304007
Murodil <murodil@cybertekschool.com>
2022-09-04 15:58:42 -0400

    bookit

--- src/test/java/karate-config.js
+++ src/test/java/karate-config.js
@@ -8,8 +8,10 @@ function fn() {
     env: env,
 	hello_world_api_url: 'https://sandbox.api.service.nhs.uk/hello-world/hello/world',
     spartan_base_url: 'http://54.211.225.58:8000/api',
-    all_spartans: '/spartans'
-  }
+    all_spartans: '/spartans',
+    bookit_base_url : 'https://cybertek-reservation-api-qa.herokuapp.com'
+
+}
   if (env == 'dev') {
     // customize
     // e.g. config.foo = 'bar';
